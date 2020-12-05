b4c7b18c4888bb8ed218a554eb684b20e7376656
Tamás PEREGI <petamas@gmail.com>
2020-12-05 10:35:43 +0100

    Day 05: simplify after binary revelation

--- src/day05.cpp
+++ src/day05.cpp
@@ -3,33 +3,14 @@
 
 namespace day05 {
 
-struct pos {
-    std::pair<int, int> row;
-    std::pair<int, int> col;
-
-    int id() const {
-        assert(row.second == row.first + 1);
-        assert(col.second == col.first + 1);
-        return row.first * 8 + col.first;
-    }
-};
-
 int64_t solve(std::istream& is, Task task) {
-    auto input = aoc::to_vector(boost::istream_range<std::string>(is));
+    auto input = boost::istream_range<std::string>(is);
 
     std::vector<int> seats;
     boost::transform(input, std::back_inserter(seats), [](const std::string& s) {
-        return boost::accumulate(s, pos{ {0,128}, {0,8} },
-            [](const pos& p, char ch) -> pos {
-                switch (ch) {
-                case 'F': return { {p.row.first, (p.row.first + p.row.second) / 2}, p.col };
-                case 'B': return { {(p.row.first + p.row.second) / 2, p.row.second}, p.col };
-                case 'L': return { p.row, {p.col.first, (p.col.first + p.col.second) / 2} };
-                case 'R': return { p.row, {(p.col.first + p.col.second) / 2, p.col.second} };
-                default: return p;
-                }
-            }
-        ).id();
+        return boost::accumulate(s, 0, [](int acc, char ch) {
+            return 2 * acc + (ch == 'B' || ch == 'R');
+        });
     });
 
     if (task == Task::FIRST) {
